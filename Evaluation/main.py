1b07603fa88740c36e472dd8cbebc896d796cbf1
MaxwellAlan <alanmaxwell@163.com>
2018-04-26 01:03:33 +0800

    add drive distance, drive time, min max norm

--- Evaluation/main.py
+++ Evaluation/main.py
@@ -6,11 +6,12 @@ import pandas as pd
 import time
 from sklearn.model_selection import train_test_split
 import lightgbm as lgb
+from math import sqrt
 
-path_train = "/data/dm/train.csv"  # 训练文件
-path_test = "/data/dm/test.csv"  # 测试文件
-# path_train = "../resource/PINGAN-2018-train_demo.csv"
-# path_test = "../resource/PINGAN-2018-test_demo.csv"
+# path_train = "/data/dm/train.csv"  # 训练文件
+# path_test = "/data/dm/test.csv"  # 测试文件
+path_train = "../resource/PINGAN-2018-train_demo.csv"
+path_test = "../resource/PINGAN-2018-test_demo.csv"
 
 path_test_out = "model/"  # 预测结果输出路径为model/xx.csv,有且只能有一个文件并且是CSV格式。
 
@@ -20,17 +21,18 @@ all_feature_list = ['CALLSTATE', 'DIRECTION', 'HEIGHT', 'LATITUDE', 'LONGITUDE',
        'TERMINALNO', 'TIME', 'TRIP_ID', 'Y', 'time', 'date', 'hour', 'minute',
        'trip_max', 'lon_max', 'lon_min', 'lon', 'lat_max', 'lat_min', 'lat',
        'heg_max', 'heg_min', 'heg_mean', 'heg', 'vol', 'sp_max', 'sp_mean',
-       'call0', 'call1', 'call_ratio_0', 'call_ratio_1']
+       'call0', 'call1', 'call_ratio_0', 'call_ratio_1','dis']
 
-use_feature_list = ['time', 'date', 'hour', 'minute',
+use_feature_list = [
        'trip_max', 'lon_max', 'lon_min', 'lon', 'lat_max', 'lat_min', 'lat',
        'heg_max', 'heg_min', 'heg_mean', 'heg', 'vol', 'sp_max', 'sp_mean',
-       'call_ratio_0', 'call_ratio_1']
+       'call_ratio_0', 'call_ratio_1', 'dis', 'ave_dri_time', 'dri_time']
 
 def load_data(path_train,path_test):
     train_data = pd.read_csv(path_train)
     test_data = pd.read_csv(path_test)
     return train_data,test_data
+
 
 def read_csv():
     """
@@ -103,6 +105,7 @@ def process():
     feature = pd.merge(feature, lonmax, how='left', on='TERMINALNO')
     feature = pd.merge(feature, lonmin, how='left', on='TERMINALNO')
     feature['lon'] = feature['lon_max'] - feature['lon_min']
+    del lonmax,lonmin
 
     # lat_max lat_min lat
     latmax = pd.DataFrame()
@@ -114,6 +117,7 @@ def process():
     feature = pd.merge(feature, latmax, how='left', on='TERMINALNO')
     feature = pd.merge(feature, latmin, how='left', on='TERMINALNO')
     feature['lat'] = feature['lat_max'] - feature['lat_min']
+    del latmax,latmin
 
     # heg_max heg_min heg_mean heg
     hegmax = pd.DataFrame()
@@ -129,6 +133,7 @@ def process():
     feature = pd.merge(feature, hegmin, how='left', on='TERMINALNO')
     feature = pd.merge(feature, hegmean, how='left', on='TERMINALNO')
     feature['heg'] = feature['heg_max'] - feature['heg_min']
+    del hegmax,hegmean,hegmin
 
     # volu 活动区间体积
     feature['vol'] = feature['lon'] * feature['lat'] * feature['heg']
@@ -142,6 +147,7 @@ def process():
         ['TERMINALNO', 'SPEED']]
     feature = pd.merge(feature, spmax, how='left', on='TERMINALNO')
     feature = pd.merge(feature, spmean, how='left', on='TERMINALNO')
+    del spmax,spmean
 
     # callstate
     call0 = pd.DataFrame()
@@ -159,6 +165,7 @@ def process():
     feature['call1'].fillna(0, inplace=True)
     feature['call_ratio_0'] = feature['call0'] / (feature['call0'] + feature['call1'])
     feature['call_ratio_1'] = feature['call1'] / (feature['call0'] + feature['call1'])
+    del call0,call1
 
     print("Feature End. feature shape:"+str(feature.shape))
     print("generate train & test set")
@@ -166,6 +173,75 @@ def process():
     test_data.drop_duplicates(subset='TERMINALNO', inplace=True)
     data.drop_duplicates(inplace=True, subset='TERMINALNO')
     print("train shape:"+str(train_data.shape)+" test shape:"+str(test_data.shape)+" data shape:"+str(data.shape))
+
+    # 行程
+    # 对每个 USER 按 TIME 排序
+    sortdata = data.sort_values(['TERMINALNO', 'time']).reset_index(drop=True)
+    # 删除TRIP_ID后去重
+    del sortdata['TRIP_ID']
+    sortdata.drop_duplicates(inplace=True)
+    # 计算经纬度差
+    sortdata['difflat'] = sortdata.groupby(['TERMINALNO'])['LATITUDE'].diff()
+    sortdata['difflon'] = sortdata.groupby(['TERMINALNO'])['LONGITUDE'].diff()
+    # 对每个用户的第一个经纬度差置0
+    sortdata.fillna(0.0, inplace=True)
+    # 计算单个距离
+    sortdata['dis2'] = sortdata['difflat'] ** 2 + sortdata['difflon'] ** 2
+    sortdata['dis'] = sortdata['dis2'].apply(sqrt)
+    del sortdata['dis2']
+    # 计算总行程
+    # disdata = pd.DataFrame()
+    # disdata[['TERMINALNO','dis']]=sortdata['dis'].groupby(['TERMINALNO']).sum()
+    disdata = sortdata['dis'].groupby(sortdata['TERMINALNO']).sum().reset_index()
+    feature = pd.merge(feature, disdata, how='left', on='TERMINALNO')
+    del sortdata,disdata
+
+    # 驾驶时长
+    # 1.去重
+    dri_time = data[['TERMINALNO', 'TIME', 'TRIP_ID']]
+    dri_time.drop_duplicates(subset=['TERMINALNO', 'TIME'], inplace=True)
+    # 2.按 TERMINALNO 和 time 排序
+    dri_time.sort_values(['TERMINALNO', 'TIME'], inplace=True)
+    dri_time['diff_time'] = dri_time.groupby(['TERMINALNO'])['TIME'].diff()
+    dri_time.fillna(0.0, inplace=True)
+    # 3.时间换算
+    dri_time['diff_time'] = dri_time['diff_time'].apply(lambda x: x / 60)
+
+    # 4.如果时间间隔大于20分钟则按新行程处理，置0
+    def f(x):
+        if x >= 20:
+            return 0
+        else:
+            return x
+
+    dri_time['diff_time'] = dri_time['diff_time'].apply(f)
+    # 5.计算驾驶总时长
+    dri_t = pd.DataFrame()
+    dri_t[['TERMINALNO', 'dri_time']] = dri_time['diff_time'].groupby(dri_time['TERMINALNO']).sum().reset_index()[
+        ['TERMINALNO', 'diff_time']]
+    feature = pd.merge(feature, dri_t, how='left', on='TERMINALNO')
+    # 6.平均时长
+    feature['ave_dri_time'] = feature['dri_time'] / feature['trip_max']
+    # 7.用户单段最大驾驶时长
+    del dri_t, dri_time
+    # use_feature_list = [
+    #     'trip_max', 'lon_max', 'lon_min', 'lon', 'lat_max', 'lat_min', 'lat',
+    #     'heg_max', 'heg_min', 'heg_mean', 'heg', 'vol', 'sp_max', 'sp_mean',
+    #     'call_ratio_0', 'call_ratio_1', 'dis', 'ave_dri_time', 'dri_time']
+
+    # 归一化
+    feature['lon'] = feature['lon'].apply(
+        lambda x: (x - feature['lon'].min()) / (feature['lon'].max() - feature['lon'].min()))
+    feature['lat'] = feature['lat'].apply(
+        lambda x: (x - feature['lat'].min()) / (feature['lat'].max() - feature['lat'].min()))
+    feature['vol'] = feature['vol'].apply(
+        lambda x: (x - feature['vol'].min()) / (feature['vol'].max() - feature['vol'].min()))
+    feature['ave_dri_time'] = feature['ave_dri_time'].apply(
+        lambda x: (x - feature['ave_dri_time'].min()) / (feature['ave_dri_time'].max() - feature['ave_dri_time'].min()))
+    feature['dri_time'] = feature['dri_time'].apply(
+        lambda x: (x - feature['dri_time'].min()) / (feature['dri_time'].max() - feature['dri_time'].min()))
+    feature['dis'] = feature['dis'].apply(
+        lambda x: (x - feature['dis'].min()) / (feature['dis'].max() - feature['dis'].min()))
 
     # 切割训练集和测试集
     train = data[0:len(train_data)]
@@ -177,10 +253,20 @@ def process():
     train_train, train_val = train_test_split(train, test_size=0.2, random_state=42)
     print("train_train_shape:"+str(train_train.shape)+"  train_val_shape:"+str(train_val.shape))
 
+    train_train['Y'] = train_train['Y'].apply(
+        lambda x: ((x - train_train['Y'].min())/(train_train['Y'].max()-train_train['Y'].min()))
+    )
+    train_val['Y'] = train_val['Y'].apply(
+        lambda x: ((x - train_val['Y'].min()) / (train_val['Y'].max() - train_val['Y'].min()))
+    )
+
     #模型训练
-    lgbmodel = lgb.LGBMRegressor(num_leaves=63, max_depth=7, n_estimators=20000, n_jobs=20)
+    lgbmodel = lgb.LGBMRegressor(num_leaves=31, max_depth=7, n_estimators=20000,learning_rate=0.001,n_jobs=20)
     lgbmodel.fit(X=train_train[use_feature_list], y=train_train['Y'],
-                 eval_set=(train_val[use_feature_list], train_val['Y']), early_stopping_rounds=200)
+                 eval_set=(train_val[use_feature_list], train_val['Y']), early_stopping_rounds=200,verbose=False)
+
+    fea_imp = pd.Series(lgbmodel.feature_importances_,use_feature_list).sort_values(ascending=False)
+    print(fea_imp)
 
     test['Pred'] = lgbmodel.predict(test[use_feature_list])
     test['Id'] = test['TERMINALNO'] - TRAIN_ID_MAX
